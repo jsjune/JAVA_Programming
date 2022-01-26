fe6af3e6eb33b4abd750556e79fd703ade0c461e
jsjune <avraskio@gmail.com>
2022-01-27 00:25:36 +0900

    update

--- src/Baekjoon/Bj2309.java
+++ src/Baekjoon/Bj2309.java
@@ -6,31 +6,34 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Bj2309 {
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int[] members = new int[9];
-		int[] sevenMembers = new int[7];
-		int sum=0;
-		
+		int sum = 0;
+		int spy1 = 0, spy2 = 0;
+
 		for (int i = 0; i < members.length; i++) {
 			members[i] = Integer.parseInt(br.readLine());
+			sum += members[i];
 		}
-		
+
 		Arrays.sort(members);
-		
-		for(int i=0;i<members.length;i++) {
-			sum+=members[i];
-			if(sum>100) {
-				break;
+
+		for (int i = 0; i < members.length - 1; i++) {
+			for (int j = i+1; j < members.length; j++) {
+				if (sum - members[i] - members[j] == 100) {
+					spy1 = i;
+					spy2 = j;
+					break;
+				}
 			}
-			if(sevenMembers.length>7) {
-				break;
-			}
-			sevenMembers[i]=members[i];
 		}
-		for(int i:sevenMembers) {
-			System.out.println(i);
+
+		for (int i = 0; i < members.length; i++) {
+			if (i == spy1 || i == spy2) 
+				continue;
+			System.out.println(members[i]);
 		}
 	}
 }
